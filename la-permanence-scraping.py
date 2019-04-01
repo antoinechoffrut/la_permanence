1e712216d7044e60a496d6d336635b1261328e52
Antoine Choffrut <antoine.choffrut@gmail.com>
2019-04-01 21:25:47 +0100

    Add backing up of data files after scraping
    Script la-permanence-scraping.py now backs up datafile attendance.csv
    - hourly
    - daily
    - weekly
    
    On branch master
    Changes to be committed:
    	modified:   la-permanence-scraping.py
    
    Changes not staged for commit:
    	modified:   attendance.csv
    	modified:   la-permanence-decision-trees.ipynb
    	modified:   la-permanence-random-forests-feature-importance.ipynb
    	modified:   la-permanence-random-forests-forecasting.ipynb
    	modified:   la-permanence-random-forests-parameters.ipynb
    	modified:   la-permanence-seasonality.ipynb
    	modified:   la-permanence-understanding.ipynb
    	modified:   moulin-availability.png
    	modified:   moulin-average-availability.png

--- la-permanence-scraping.py
+++ la-permanence-scraping.py
@@ -6,7 +6,7 @@
 # IMPORTS
 import os
 import re
-#import time
+import shutil
 import datetime
 import pytz
 import logging
@@ -20,17 +20,15 @@ SEP = ","                                # separator for .csv file
 SCRIPT_NAME = os.path.basename(__file__)
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-HOME_DIR = os.path.expanduser("~")
-PROJECT_DIR = SCRIPT_DIR
-ARCHIVE_DIR = os.path.join(
-    PROJECT_DIR,
-    "ARCHIVES"
-)
-BACKUP_DIR = os.path.join(
-    PROJECT_DIR,
-    "BACKUP"
-)
 
+PROJECT_DIR = SCRIPT_DIR
+BACKUP_DIR = os.path.join(PROJECT_DIR, 'DATA_BACKUPS')
+
+DATAFILE_NAME = 'attendance.csv'
+DATAFILE_PATH = os.path.join(
+    PROJECT_DIR,
+    DATAFILE_NAME  # "attendance.csv"
+)
 
 # LOGGING
 SCRIPT_LOG = re.sub(".py$", ".log", SCRIPT_NAME)
@@ -56,6 +54,49 @@ FMT_LOG = '%a %d %b %Y %H:%M:%S %Z%z'  # for logging
 # CLASS DEFINITIONS
 
 # FUNCTION DEFINITIONS
+def dateparse(str):
+    return pd.datetime.strptime(str, '%Y-%m-%d-%H-%M-%S')
+
+
+def ts_roundoff(ts, freq='1H'):
+    """Convert timetamp to UTC standard and return round off."""
+    ts = ts.tz_convert(TZ_UTC)
+    result = ts
+    if freq == '1H':
+        result = pd.Timestamp(
+            ts.year,
+            ts.month,
+            ts.day,
+            ts.hour
+        )
+    elif freq == '1D':
+        result = pd.Timestamp(
+            ts.year,
+            ts.month,
+            ts.day
+        )
+    elif freq == '1W':
+        result = ts
+        res_str = ts.strftime('%G-W%V-1')  # this week's first day
+        res_dt = datetime.datetime.strptime(res_str, ('%G-W%V-%u'))
+        res_ts = \
+            pd.Timestamp(
+                res_dt.year,
+                res_dt.month,
+                res_dt.day
+            ).tz_localize(TZ_UTC)
+        result = res_ts
+
+    return result
+
+
+def is_time_to_backup(run_time_ts, last_ts, freq='1H'):
+    """Return True if it is time to backup dataset"""
+    if freq == '1W':
+        freq = '7D'
+
+    return ts_roundoff(run_time_ts, freq) \
+        >= ts_roundoff(last_ts, freq) + pd.Timedelta(freq)
 
 
 def main():
@@ -76,14 +117,15 @@ def main():
     LOGGER.info("Logfile: {}".format(SCRIPT_LOG))
 
     # GET DATAFRAME FROM CSV FILE - IF EXISTS, OR ELSE CREATE
-    foutname = os.path.join(
-        PROJECT_DIR,
-        "attendance.csv"
-    )
-    if not os.path.exists(foutname):
+    LOGGER.info("Datafile: {0}".format(DATAFILE_PATH))
+    LOGGER.info("Backup files in: {0}".format(BACKUP_DIR))
+
+    if not os.path.exists(DATAFILE_PATH):
+        LOGGER.info("Datafile {0} does not exist".format(DATAFILE_NAME))
         df = pd.DataFrame(columns=["timestamp", "Moulin", "Alésia"])
     else:
-        df = pd.read_csv(foutname, sep=SEP)
+        LOGGER.info("Datafile: {0} already exists".format(DATAFILE_NAME))
+        df = pd.read_csv(DATAFILE_PATH, sep=SEP)
 
     # GET PAGE
     url = "https://www.la-permanence.com"
@@ -132,17 +174,82 @@ def main():
     )
 
     # WRITE DATAFRAME INTO CSV FILE
-    df.to_csv(foutname, sep=SEP, index=False)
+    LOGGER.info("Writing data to: {0}".format(DATAFILE_NAME))
+    df.to_csv(DATAFILE_PATH, sep=SEP, index=False)
+
+    # BACKUP FILE - IF TIME TO DO SO
+    LOGGER.info(
+        "Check whether backup files need updating."
+        )
+    run_time_ts = pd.Timestamp(
+        run_time.year,
+        run_time.month,
+        run_time.day,
+        run_time.hour,
+        run_time.minute,
+        run_time.second
+    ).tz_localize(TZ_UTC)
+
+    for (freq, mode) in [('1H', 'hourly'), ('1D', 'daily'), ('1W', 'weekly')]:
+        LOGGER.info("Backup mode: {0}".format(mode))
+
+        backup_name = ".".join([
+            "-".join([
+                ".".join(DATAFILE_NAME.split('.')[0:-1]),
+                "BACKUP",
+                mode
+            ]),
+            DATAFILE_NAME.split('.')[-1]
+        ])
+        backup_path = os.path.join(BACKUP_DIR, backup_name)
+
+        if not os.path.exists(backup_path):
+            shutil.copy(DATAFILE_PATH, backup_path)
+            LOGGER.info(
+                "Backup file {0} did not exist, now created.".format(
+                    backup_name
+                )
+            )
+        else:
+
+            df_backup = pd.read_csv(
+                backup_path,
+                sep=',',
+                dtype={'Moulin': np.uint8, 'Alésia': np.uint8},
+                parse_dates=['timestamp'],
+                date_parser=dateparse
+            )
+            df_backup['timestamp'] = \
+                df_backup['timestamp'].apply(
+                    lambda ts: ts.tz_localize(TZ_UTC)
+                    )
+
+            last_ts = df_backup['timestamp'].max()
+
+            if is_time_to_backup(run_time_ts, last_ts, freq):
+                LOGGER.info("Time to update {0}".format(backup_name))
+                shutil.copy(DATAFILE_PATH, backup_path)
+            else:
+                LOGGER.info("Not yet time to update {0}".format(backup_name))
+
+        
+                
 
     # CONCLUDING SCRIPT
-    end_of_script = time.time()
+
+    end_of_script = datetime.datetime.now(tz=TZ_UTC)
 
     LOGGER.info(
-        "Time stamp: {0} (duration: {1:.2f} sec)".format(
-            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_of_script)),
-            (end_of_script - run_time)
+        "End of script: {0}".format(
+            end_of_script.strftime(FMT_LOG)
         )
     )
+    # LOGGER.info(
+    #     "Time stamp: {0} (duration: {1:.2f} sec)".format(
+    #         datetime.strftime("%Y-%m-%d %H:%M:%S", datetime.localtime(end_of_script)),
+    #         (end_of_script - run_time)
+    #     )
+    # )
 
     LOGGER.info("End of script {}".format(SCRIPT_NAME))
 
